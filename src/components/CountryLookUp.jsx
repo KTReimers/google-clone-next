dc94a7012276035d01b54c90aba023ca612d982c
Kyle <reimers95@gmail.com>
2024-01-17 18:40:01 -0600

    connected to google API for search terms

--- src/components/CountryLookUp.jsx
+++ src/components/CountryLookUp.jsx
@@ -7,6 +7,9 @@ export default function CountryLookUp() {
         fetch(`https://api.ipdata.co?api-key=${process.env.NEXT_PUBLIC_IP_API_KEY}`).then((res) => res.json()).then((data) => setCountry(data.country_name))
     },[]);
     return (
-        <div>{country}</div>
+        <div className="flex justify-between">
+            <div className="">{country}</div>
+            <div className="">This is not the actual Google website! This is a clone project</div>
+        </div>
     )
 }
