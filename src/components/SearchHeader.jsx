4bfab36caa8bf8646b1d2f3532e2f3a7c3473031
Kyle <reimers95@gmail.com>
2024-01-19 17:44:15 -0600

    Adjusted search header to be more mobile friendly

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -9,10 +9,13 @@ import SearchHeaderOptions from './SearchHeaderOptions';
 export default function SearchHeader() {
     return (
         <header className="sticky top-0 bg-white">
-            <div className="flex items-center w-full p-6 justify-between">
+            <div className="flex items-center w-full p-6 justify-between sm:flex-row flex-col">
+                <div className="flex justify-between w-full sm:w-[150px] pb-2">
                 <Link href={"/"}>
                     <Image width="120" height="40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png" alt="google icon"/>
                 </Link>
+                <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2 sm:hidden">Sign In</button>
+                </div>
                 <div className="flex-1">
                     <SearchBox/>
                 </div>
@@ -20,7 +23,7 @@ export default function SearchHeader() {
                     <RiSettings3Line className="header-icon"/>
                     <TbGridDots className="header-icon"/>
                 </div>
-                <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2">Sign In</button>
+                <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2 hidden sm:flex">Sign In</button>
             </div>
             <SearchHeaderOptions />
         </header>
